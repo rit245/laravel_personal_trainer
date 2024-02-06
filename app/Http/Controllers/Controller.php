c8b4dfeafcee7ee598e9308a4e3f7e63013e9b0e
rit245 <urit245@gmail.com>
2024-02-06 22:40:37 +0900

    [#1] Create Swagger Package

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     title="Laravel Home Traning API",
+ *     version="1.0"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
